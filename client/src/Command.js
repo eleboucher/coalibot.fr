53c4b607e9e9a8c2289d67b9267156977397f78b
Erwan LEBOUCHER <elebouch@e1r2p13.42.fr>
2018-03-10 18:01:00 +0100

    because pink is pretty

--- client/src/Command.js
+++ client/src/Command.js
@@ -30,12 +30,12 @@ class Command extends Component {
         {
           label: 'Commands',
           data: data,
-          backgroundColor: 'rgba(179,181,198,0.2)',
-          borderColor: 'rgba(179,181,198,1)',
-          pointBackgroundColor: 'rgba(179,181,198,1)',
+          backgroundColor: 'rgba(255,99,132,0.2)',
+          borderColor: 'rgba(255,99,132,1)',
+          pointBackgroundColor: 'rgba(255,99,132,1)',
           pointBorderColor: '#fff',
           pointHoverBackgroundColor: '#fff',
-          pointHoverBorderColor: 'rgba(179,181,198,1)'
+          pointHoverBorderColor: 'rgba(255,99,132,1)'
         }
       ]
     }
