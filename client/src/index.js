1b3a1bc2abad8c17af470343f9d978789556c548
Erwan LEBOUCHER <elebouch@e3r11p19.42.fr>
2018-03-12 15:29:06 +0100

    update

--- client/src/index.js
+++ client/src/index.js
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 import Lastest from './Lastest'
-import registerServiceWorker from './registerServiceWorker'
 
 ReactDOM.render(<App />, document.getElementById('app'))
 ReactDOM.render(<Lastest />, document.getElementById('lastest'))
-//registerServiceWorker()
