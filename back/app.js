6cd7145116f1153e2276a44d2087945cfc3d70ef
Erwan Leboucher <erwanleboucher@gmail.com>
2018-12-02 19:50:33 +0100

    fix passport 42

--- back/app.js
+++ back/app.js
@@ -4,9 +4,10 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const passport = require('passport');
-const User = require('./models/user');
-const indexRouter = require('./routes/index');
 const FortyTwoStrategy = require('passport-42').Strategy;
+const session = require('express-session');
+const models = require('./models');
+const indexRouter = require('./routes/index');
 
 const app = express();
 
@@ -21,15 +22,33 @@ passport.use(
       clientID: process.env.FORTYTWO_APP_ID,
       clientSecret: process.env.FORTYTWO_APP_SECRET,
       callbackURL: process.env.FORTYTWO_APP_CALLBACK,
+      profileFields: {
+        username: 'login',
+      },
     },
     (accessToken, refreshToken, profile, cb) => {
-      User.findOrCreate({ where: { login: profile.login } }, (err, user) => cb(err, user));
+      models.User.findOrCreate({ where: { login: profile.username } })
+        .spread(user => cb(null, user))
+        .catch(err => cb(err, null));
     },
   ),
 );
 
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+  // if you use Model.id as your idAttribute maybe you'd want
+  // done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  models.User.findById(id)
+    .then(user => done(null, user))
+    .catch(err => done(err, null));
+});
+
+app.use(session({ secret: 'keyboard cat' }));
+
 app.use(passport.initialize());
-app.use(passport.session());
 app.use(passport.session());
 
 app.use('/', indexRouter);
