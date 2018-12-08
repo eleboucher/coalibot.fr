1f9b4e1013c49d7bf170bbe0a73f40a38cbcf73e
Erwan LEBOUCHER <elebouch@e3r4p15.42.fr>
2018-12-08 17:49:52 +0100

    use exciting collection

--- back/models/leaderboard.js
+++ back/models/leaderboard.js
@@ -1,10 +1,13 @@
 const mongoose = require('mongoose');
 
-const leaderboardSchema = new mongoose.Schema({
-  login: String,
-  level: Number,
-  rentree: String,
-  coalition: String,
-});
+const leaderboardSchema = new mongoose.Schema(
+  {
+    login: String,
+    level: Number,
+    rentree: String,
+    coalition: String,
+  },
+  { collection: 'Leaderboard' },
+);
 
 module.exports = mongoose.model('Leaderboard', leaderboardSchema);
