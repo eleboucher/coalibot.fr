4c8235e8440c642b060e57219cfdab16dcde398b
Erwan Leboucher <erwanleboucher@gmail.com>
2018-12-09 18:28:19 +0100

    route api as /api/

--- back/app.js
+++ back/app.js
@@ -50,7 +50,7 @@ app.use(session({ secret: 'keyboard cat' }));
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use('/', indexRouter);
+app.use('/api/', indexRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
@@ -62,7 +62,6 @@ app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
   // render the error page
   res.status(err.status || 500);
   res.send(res.locals);
