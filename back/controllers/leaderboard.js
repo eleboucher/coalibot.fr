d0209982b6caa7997a4f612873fc087306311946
Erwan Leboucher <erwanleboucher@gmail.com>
2019-01-24 22:04:58 +0100

    sort by level by default

--- back/controllers/leaderboard.js
+++ back/controllers/leaderboard.js
@@ -2,14 +2,14 @@ const Leaderboard = require('../models/leaderboard');
 
 const getLeaderboardByYear = async (req, res) => {
   const { rentree } = req.params;
-  const ret = await Leaderboard.find({ rentree });
+  const ret = await Leaderboard.find({ rentree }).sort({ level: -1 });
   return res.json(ret);
 };
 
 const getLeaderboardByCoalition = async (req, res) => {
   let { coalition } = req.params;
   coalition = coalition.charAt(0).toUpperCase();
-  const ret = await Leaderboard.find({ coalition });
+  const ret = await Leaderboard.find({ coalition }).sort({ level: -1 });
   return res.json(ret);
 };
 
