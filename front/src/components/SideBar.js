083dcff2620c5568ad741e7847492d9dfda451e3
Erwan Leboucher <erwanleboucher@gmail.com>
2019-01-26 17:27:58 +0100

    side bar design

--- front/src/components/SideBar.js
+++ front/src/components/SideBar.js
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
-import Grid from "./Grid";
+import { Link as RawLink } from "react-router-dom";
+
 import Logout from "./Logout";
 
 const Wrapper = styled.aside`
@@ -12,9 +13,12 @@ const Wrapper = styled.aside`
   bottom: 0px;
   display: flex;
   flex-direction: column;
+  justify-content: space-between;
   box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
   background-color: var(--lightPrimary);
   color: var(--lightText);
+  padding: 12px 19px 2px;
+
   @media (max-width: 768px) {
     left: 0px;
     right: 0px;
@@ -23,38 +27,76 @@ const Wrapper = styled.aside`
     width: auto;
     align-items: center;
     flex-direction: row;
+    padding: 0px;
+    flex-wrap: wrap;
   }
 `;
 
-const Links = styled.div`
+const Title = styled.span`
+  text-align: center;
+  font-size: 24px;
+  font-weight: 800px;
   align-self: center;
-  display: flex;
+  margin-bottom: 2rem;
 
-  flex-direction: inherit;
-  justify-content: space-between;
-
-  align-items: center;
+  @media (max-width: 768px) {
+    margin: 0;
+  }
 `;
 
-const Title = styled.div`
-  position: relative;
-  /* margin-top: 2rem; */
+const Bottom = styled.div`
   align-self: center;
-  /* margin-bottom: 100px; */
+  margin-bottom: 2rem;
+  @media (max-width: 768px) {
+    margin: 0;
+  }
+`;
+
+const Link = styled(RawLink)`
+  text-decoration: none;
+  color: var(--lightText);
+`;
+
+const NavLink = styled.div`
+  margin: 8px 0;
+
+  @media (max-width: 768px) {
+    margin: 0 0 0 0.5rem !important;
+  }
+`;
+
+const Head = styled.div`
+  margin-top: 2rem;
+  display: flex;
+  flex-direction: inherit;
+  flex: 1 1 0%;
+
+  @media (max-width: 768px) {
+    margin: 0;
+    & > ${Link} {
+      align-self: center;
+    }
+  }
 `;
 
 const SideBar = ({ children }) => {
   return (
     <>
       <Wrapper>
-        <Title>
-          <span>CoaliZone</span>
-        </Title>
-        <Links>
-          <span>Home</span>
-          <span>LeaderBoard</span>
+        <Head>
+          <Title as={Link} to="/">
+            CoaliZone
+          </Title>
+          <NavLink as={Link} to="/">
+            Home
+          </NavLink>
+          <NavLink as={Link} to="/dashboard">
+            LeaderBoard
+          </NavLink>
+        </Head>
+        <Bottom>
           <Logout />
-        </Links>
+        </Bottom>
       </Wrapper>
     </>
   );
