f1fc44e6ba7cba553d5cb13eb66169ce01aefcf3
Erwan Leboucher <erwanleboucher@gmail.com>
2019-03-24 19:09:46 +0100

    add new models to admin view

--- api/admin/__init__.py
+++ api/admin/__init__.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from api.models.user import User
+from api.models import User, CursusUser, Student, Coalition, CoalitionUser, Cursus
 
 
 admin.site.register(User, UserAdmin)
+admin.site.register(CursusUser)
+admin.site.register(Student)
+admin.site.register(Coalition)
+admin.site.register(Cursus)
+admin.site.register(CoalitionUser)
