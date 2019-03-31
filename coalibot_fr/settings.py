8998b4c1dc02eb3c2a269bfca866cc8b19f6333b
Erwan Leboucher <erwanleboucher@gmail.com>
2019-03-31 16:16:27 +0200

    update django settings

--- coalibot_fr/settings.py
+++ coalibot_fr/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", "!!!SET A KEY!!!")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
