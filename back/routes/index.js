63819f08189df05d81004aa66aeca00b5fdf6a7b
Erwan Leboucher <erwanleboucher@gmail.com>
2020-12-17 10:15:07 +0100

    add middleware for the cursus route

--- back/routes/index.js
+++ back/routes/index.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const auth = require("./auth");
 const cursus = require("./cursus");
-
+const islogged = require("../middlewares/is_logged");
 const router = express.Router();
 
 router.use("/auth", auth);
-router.use("/cursus", cursus);
+router.use("/cursus", islogged, cursus);
 
 module.exports = router;
